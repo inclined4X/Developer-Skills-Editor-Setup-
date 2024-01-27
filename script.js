31de6e3cc1208c9cfd0fbe7821a6fc77472cc9fe
inclined4X <jezemiahsam48@gmail.com>
2024-01-27 23:35:46 +0000

    simple temperature calc

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 23) console.log(23);
+/* const x = '23';
 
 const calcAge = birthYear => 2037 - birthYear;
+
+// user snippet
+console.log(calcAge(1991));
+*/
+
+// Problem
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
+calcTempAmplitude(3, 7, 4, 23);
