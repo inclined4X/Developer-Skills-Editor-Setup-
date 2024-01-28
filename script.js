3c4b555229fbbb76929bddf23c59634a1809b112
inclined4X <jezemiahsam48@gmail.com>
2024-01-28 00:06:37 +0000

    finished up 2nd problem which has two arrays, which I had to merge

--- script.js
+++ script.js
@@ -10,8 +10,34 @@ console.log(calcAge(1991));
 */
 
 // Problem
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-const calcTempAmplitude = function (temps) {
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
+
+//problem 2
+//funcion should now recieve 2 arrays of temps
+
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
   let max = temps[0];
   let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
@@ -24,5 +50,5 @@ const calcTempAmplitude = function (temps) {
   return max - min;
 };
 
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
